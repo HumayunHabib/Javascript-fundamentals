5458f70c6bf764200a83d91357e7e47f8dfd73b9
HumayunHabib <humayunhabib786@gmail.com>
2023-11-20 18:11:32 +0500

    max value

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,15 @@
 // Remember, we're gonna use strict mode in all scripts now!
-'use strict';
+"use strict";
 
+const temperatures = [3, -2, -6, -1, 3, 9, 13, 17, 15, 14, 9, 5];
+const calctempAmplitude = function name(tempArr) {
+  let max = tempArr[0];
+  for (let i = 0; i < tempArr.length; i++) {
+    if (tempArr[i] > max) {
+      max = tempArr[i];
+    }
+  }
+  console.log(max);
+};
+
+calctempAmplitude(temperatures);
