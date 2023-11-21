50fb605058935a1b0fd3ddd98c5795b904baf98c
HumayunHabib <humayunhabib786@gmail.com>
2023-11-21 22:13:41 +0500

    printForeCast

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -64,20 +64,38 @@ const measureKelvin = function name() {
 
 // Using a debugger
 
-const calcTempAmplitudeBug = (t1, t2) => {
-  const temps = t1.concat(t2);
-  console.log(temps);
-  let max = 0;
-  let min = 0;
-  for (let i = 0; i < temps.length; i++) {
-    const currTemp = temps[i];
-    if (typeof currTemp !== "number") continue;
+// const calcTempAmplitudeBug = (t1, t2) => {
+//   const temps = t1.concat(t2);
+//   console.log(temps);
+//   let max = 0;
+//   let min = 0;
+//   for (let i = 0; i < temps.length; i++) {
+//     const currTemp = temps[i];
+//     if (typeof currTemp !== "number") continue;
 
-    if (currTemp > max) max = currTemp;
-    if (currTemp < min) min = currTemp;
+//     if (currTemp > max) max = currTemp;
+//     if (currTemp < min) min = currTemp;
+//   }
+//   console.log(max, min);
+//   return max - min;
+// };
+// const amplitudeBug = calcTempAmplitudeBug([3, 5, 1], [9, 4, 5]);
+// console.log(amplitudeBug);
+
+const data1 = [17, 21, 23];
+const data2 = [17, 21, 23];
+
+// console.log(
+//   `${data1[0]}°C in ${1} days......${data1[1]}°C in ${2} days......${
+//     data1[2]
+//   }°C in ${3} days...`
+// );
+
+const printForeCast = function (arr) {
+  let str = "";
+  for (let i = 0; i < arr.length; i++) {
+    str += `${arr[i]}°C in ${i + 1} days...`;
   }
-  console.log(max, min);
-  return max - min;
+  console.log("...", str);
 };
-const amplitudeBug = calcTempAmplitudeBug([3, 5, 1], [9, 4, 5]);
-console.log(amplitudeBug);
+printForeCast(data1);
